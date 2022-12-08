4fe333af417b57ad961b68a242b548f807325f6e
squal94 <lounis.khadir@wanadoo.fr>
2022-12-08 21:49:40 +0100

    remise a zero du tableau

--- wealth_health/src/Pages/Employee.js
+++ wealth_health/src/Pages/Employee.js
@@ -1,11 +1,9 @@
 import React from "react";
-import { BasicTable } from "../Components/BasicTable";
 
 const Employee = () => {
   return (
     <div>
       <h1>Je suis employee</h1>
-      <BasicTable />
     </div>
   );
 };
