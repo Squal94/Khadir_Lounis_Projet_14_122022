6538e879a840a3a6305c129201e24b210bc0f9f6
squal94 <lounis.khadir@wanadoo.fr>
2022-12-11 22:15:35 +0100

    Suite du composant selectItem v1

--- wealth_health/src/Components/SelectItem.js
+++ wealth_health/src/Components/SelectItem.js
@@ -1,13 +1,24 @@
 import React from "react";
 import arrowDown from "./../Assets/img/angle-arrow-down.png";
 
-const SelectItem = () => {
+const SelectItem = ({ props }) => {
   return (
     <div className="selectItem">
-      <div id="selectField">
+      <div id="selectField" className="selectItem__field">
         <p>Select choice</p>
-        <img src={arrowDown} alt="Fleche ouverture du select" />
+        <img
+          className="selectItem__field--img"
+          src={arrowDown}
+          alt="Fleche ouverture du select"
+        />
       </div>
+      <ul>
+        {props.map((unit) => (
+          <li>
+            <p key={unit.abbreviation}>{unit.name}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
--- wealth_health/src/Pages/Employee.js
+++ wealth_health/src/Pages/Employee.js
@@ -1,11 +1,12 @@
 import React from "react";
 import SelectItem from "../Components/SelectItem";
+import states from "../Assets/State";
 
 const Employee = () => {
   return (
     <div>
       <h1>Je suis employee</h1>
-      <SelectItem />
+      <SelectItem props={states} />
     </div>
   );
 };
