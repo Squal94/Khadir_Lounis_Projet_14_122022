d830217ac62f9691979f7be24938742a2121f863
squal94 <lounis.khadir@wanadoo.fr>
2022-12-19 16:55:34 +0100

    gestion modal redux v1

--- wealth_health/src/Features/modal.slice.js
+++ wealth_health/src/Features/modal.slice.js
@@ -1,13 +1,25 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = "";
+const initialState = {
+  isOpened: false,
+  isClose: false,
+};
 
 export const modalSlice = createSlice({
   name: "modal",
   initialState: initialState,
-  reducers: {},
+  reducers: {
+    open: (state) => {
+      state.isOpened = true;
+      state.isClose = false;
+    },
+    close: (state) => {
+      state.isOpened = false;
+      state.isClose = true;
+    },
+  },
 });
 
-export const { action } = modalSlice.actions;
+export const { open, close } = modalSlice.actions;
 
 export default modalSlice.reducer;
