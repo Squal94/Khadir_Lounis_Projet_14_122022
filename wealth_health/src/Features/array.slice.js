a30116c80a00fd005428594413908923d384fa3d
squal94 <lounis.khadir@wanadoo.fr>
2022-12-21 11:53:02 +0100

    probleme de partage de state entre slice

--- wealth_health/src/Features/array.slice.js
+++ wealth_health/src/Features/array.slice.js
@@ -1,13 +1,46 @@
 import { createSlice } from "@reduxjs/toolkit";
+import Store from "./../app/Store";
+import store from "./../app/Store";
 
-const initialState = "";
+// const reduxStore = store.getState();
+// const array = reduxStore.employee;
+const initialState = [];
 
 export const arraySlice = createSlice({
   name: "array",
   initialState: initialState,
-  reducers: {},
+  reducers: {
+    test: (state) => {
+      // const reduxStore = store.getState();
+      // const array = reduxStore.employee;
+      // state.push(...array);
+      console.log(Store);
+    },
+  },
 });
 
-export const { action } = arraySlice.actions;
+export const { test } = arraySlice.actions;
 
 export default arraySlice.reducer;
+
+// import { createSlice } from "@reduxjs/toolkit";
+// import { useSelector } from "react-redux";
+
+// const initialState = "";
+
+// const newEmployeeStore = () => {
+
+//   const dataStoreEmployee = useSelector((state) => state.employee);
+
+// }
+// // const initialState = useSelector((state) => state.employee);
+
+// export const arraySlice = createSlice({
+//   name: "array",
+//   initialState: initialState,
+//   reducers: {},
+// });
+
+// export const { action } = arraySlice.actions;
+
+// export default arraySlice.reducer;
--- wealth_health/src/Pages/Employee.js
+++ wealth_health/src/Pages/Employee.js
@@ -1,11 +1,16 @@
 import React from "react";
+import { useDispatch } from "react-redux";
 import ArrayEmployee from "../Components/ArrayEmployee";
+import { test } from "../Features/array.slice";
 // import ModalSubmit from "../Components/ModalSubmit";
 // import SelectItem from "../Components/SelectItem";
 // import states from "../Assets/stateData";
 // import DatePicker from "../Components/DatePicker";
 
 const Employee = () => {
+  const dispatch = useDispatch;
+
+  dispatch(test());
   return (
     <div>
       {/* <h1>Je suis employee</h1>
