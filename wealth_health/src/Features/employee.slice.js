6ba308886a06045392bdd9f8a0afcd48cb7d9ba7
squal94 <lounis.khadir@wanadoo.fr>
2022-12-21 20:15:27 +0100

    Sort array with action redux v4 sort ok

--- wealth_health/src/Features/employee.slice.js
+++ wealth_health/src/Features/employee.slice.js
@@ -2,6 +2,8 @@ import { createSlice, current } from "@reduxjs/toolkit";
 import dataEmployee from "../Assets/Data.json";
 
 const initialState = [...dataEmployee];
+let toggle = true;
+let arrow = true;
 // console.log(initialState);
 // current
 
@@ -16,59 +18,122 @@ export const employeeSlice = createSlice({
     deleteEmployee: (state, action) => {},
     editEmployee: (state, action) => {},
     sortTest: (state, action) => {
-      let toggle = true;
-      let arrow = true;
-      let choiceColumn;
-      console.log(action.payload);
-      switch (action.payload) {
-        case "columnFirst":
-          choiceColumn = "firstName";
-          break;
-        case "columnLast":
-          choiceColumn = "lastName";
-          break;
-        case "columnStart":
-          choiceColumn = "StartDate";
-          break;
-        case "columnDepartement":
-          choiceColumn = "Departement";
-          break;
-        case "columnBirth":
-          choiceColumn = "DateofBirth";
-          break;
-        case "columnStreet":
-          choiceColumn = "street";
-          break;
-        case "columnCity":
-          choiceColumn = "city";
-          break;
-        case "columnState":
-          choiceColumn = "State";
-          break;
-        case "columnZip":
-          choiceColumn = "zipCode";
-          break;
-        default:
-          console.log(`Sorry, we are out of ${choiceColumn}.`);
-      }
-
-      if (toggle === true) {
-        state.sort(function (a, b) {
-          toggle = !toggle;
-          arrow = !arrow;
-          // console.log(arrow);
-          // console.log(toggle);
-          return a.zipCode - b.zipCode;
+      if (toggle) {
+        // eslint-disable-next-line array-callback-return
+        state.sort((a, b) => {
+          switch (action.payload) {
+            case "columnFirst":
+              if (a.firstName > b.firstName) {
+                return 1;
+              } else if (b.firstName > a.firstName) {
+                return -1;
+              } else {
+                return 0;
+              }
+            case "columnLast":
+              if (a.lastName > b.lastName) {
+                return 1;
+              } else if (b.lastName > a.lastName) {
+                return -1;
+              } else {
+                return 0;
+              }
+            case "columnStart":
+              return (
+                new Date(a.StartDate).valueOf() -
+                new Date(b.StartDate).valueOf()
+              );
+            case "columnDepartement":
+              return a.zipCode - b.zipCode;
+            case "columnBirth":
+              return (
+                new Date(a.DateofBirth).valueOf() -
+                new Date(b.DateofBirth).valueOf()
+              );
+            case "columnStreet":
+              if (a.street > b.street) {
+                return 1;
+              } else if (b.street > a.street) {
+                return -1;
+              } else {
+                return 0;
+              }
+            case "columnCity":
+              if (a.city > b.city) {
+                return 1;
+              } else if (b.city > a.city) {
+                return -1;
+              } else {
+                return 0;
+              }
+            case "columnState":
+              return a.zipCode - b.zipCode;
+            case "columnZip":
+              return a.zipCode - b.zipCode;
+            default:
+          }
         });
-      }
-      if (toggle === false) {
-        state.sort(function (a, b) {
-          toggle = !toggle;
-          arrow = !arrow;
-          // console.log(arrow);
-          // console.log(toggle);
-          return b.zipCode - a.zipCode;
+        toggle = false;
+        arrow = false;
+        console.log(arrow);
+      } else {
+        // eslint-disable-next-line array-callback-return
+        state.sort((a, b) => {
+          switch (action.payload) {
+            case "columnFirst":
+              if (b.firstName > a.firstName) {
+                return 1;
+              } else if (a.firstName > b.firstName) {
+                return -1;
+              } else {
+                return 0;
+              }
+            case "columnLast":
+              if (b.lastName > a.lastName) {
+                return 1;
+              } else if (a.lastName > b.lastName) {
+                return -1;
+              } else {
+                return 0;
+              }
+            case "columnStart":
+              return (
+                new Date(b.StartDate).valueOf() -
+                new Date(a.StartDate).valueOf()
+              );
+            case "columnDepartement":
+              return b.zipCode - a.zipCode;
+            case "columnBirth":
+              return (
+                new Date(b.DateofBirth).valueOf() -
+                new Date(a.DateofBirth).valueOf()
+              );
+            case "columnStreet":
+              if (b.street > a.street) {
+                return 1;
+              } else if (a.street > b.street) {
+                return -1;
+              } else {
+                return 0;
+              }
+            case "columnCity":
+              if (b.city > a.city) {
+                return 1;
+              } else if (a.city > b.city) {
+                return -1;
+              } else {
+                return 0;
+              }
+            case "columnState":
+              return b.zipCode - a.zipCode;
+            case "columnZip":
+              return b.zipCode - a.zipCode;
+            default:
+          }
         });
+        toggle = true;
+        arrow = true;
+        console.log(arrow);
       }
     },
     // logout: (state) => {
