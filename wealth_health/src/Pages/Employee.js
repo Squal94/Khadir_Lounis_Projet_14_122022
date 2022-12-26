a456d0f026ebbffa1ff261c7f67f8f326063ac2e
squal94 <lounis.khadir@wanadoo.fr>
2022-12-27 00:39:36 +0100

    test inclure le package npm select

--- wealth_health/src/Pages/Employee.js
+++ wealth_health/src/Pages/Employee.js
@@ -3,14 +3,14 @@ import ArrayEmployee from "../Components/ArrayEmployee";
 import NumberLine from "../Components/NumberLine";
 import Pagination from "../Components/Pagination";
 import SearchArray from "../Components/SearchArray";
-import SelectItem from "../Components/SelectItem";
-import states from "../Assets/stateData";
+// import SelectItem from "../Components/SelectItem";
+// import states from "../Assets/stateData";
 
 const Employee = () => {
   return (
     <div>
       <h1>Current Employees</h1>
-      <SelectItem props={states} />
+      {/* <SelectItem props={states} /> */}
       {/* <DatePicker title={"Date of Birth"} />  */}
       <NumberLine />
       <SearchArray />
