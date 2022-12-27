7332d4334b88a53d1b2b851aed3bfe76b63f0939
squal94 <lounis.khadir@wanadoo.fr>
2022-12-28 00:07:27 +0100

    Creation et import d'un conposant reussi

--- wealth_health/src/Pages/Employee.js
+++ wealth_health/src/Pages/Employee.js
@@ -3,16 +3,16 @@ import ArrayEmployee from "../Components/ArrayEmployee";
 import NumberLine from "../Components/NumberLine";
 import Pagination from "../Components/Pagination";
 import SearchArray from "../Components/SearchArray";
+import { SelectItem } from "@squal94/selectlkv2";
 // import SelectItem from "../Components/SelectItem";
-// import states from "../Assets/stateData";
+import states from "../Assets/stateData";
 
 const Employee = () => {
   return (
     <div>
       <h1>Current Employees</h1>
-      <selectItem />
       {/* <DatePicker title={"Date of Birth"} />  */}
-
+      <SelectItem arrayProps={states} />
       <NumberLine />
       <SearchArray />
       <ArrayEmployee />
