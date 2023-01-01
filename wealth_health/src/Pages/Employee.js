0527fa87f99cd4d5c0a0095d6a81ab90e5e2766c
squal94 <lounis.khadir@wanadoo.fr>
2023-01-02 00:42:07 +0100

    test new lib

--- wealth_health/src/Pages/Employee.js
+++ wealth_health/src/Pages/Employee.js
@@ -3,6 +3,9 @@ import ArrayEmployee from "../Components/ArrayEmployee";
 import NumberLine from "../Components/NumberLine";
 import Pagination from "../Components/Pagination";
 import SearchArray from "../Components/SearchArray";
+// import { SelectItem } from "@squal94/selectprojet14ocrv3";
+// import arrowImg from "./../Assets/img/angle-arrow-down.png";
+// import departement from "../Assets/departementData";
 
 const Employee = () => {
   return (
@@ -16,6 +19,11 @@ const Employee = () => {
       <div className="EmployeeContainer__pagination">
         <Pagination />
       </div>
+      {/* <SelectItem
+        arrayProps={departement}
+        selectImg={arrowImg}
+        selectId="departement"
+      /> */}
     </div>
   );
 };
