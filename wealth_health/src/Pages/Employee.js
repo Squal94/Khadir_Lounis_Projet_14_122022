d7e22706fa882e357027a46b5cb444fe5a1d17e6
squal94 <lounis.khadir@wanadoo.fr>
2023-01-04 15:32:45 +0100

    commentaires v3

--- wealth_health/src/Pages/Employee.js
+++ wealth_health/src/Pages/Employee.js
@@ -3,9 +3,6 @@ import ArrayEmployee from "../Components/ArrayEmployee";
 import NumberLine from "../Components/NumberLine";
 import Pagination from "../Components/Pagination";
 import SearchArray from "../Components/SearchArray";
-// import { SelectItem } from "@squal94/selectprojet14ocrv3";
-// import arrowImg from "./../Assets/img/angle-arrow-down.png";
-// import departement from "../Assets/departementData";
 
 const Employee = () => {
   return (
